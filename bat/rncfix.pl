b5f780c4631e780c6ab5f07efd95a087ca45cc24
Vladimir Alexiev <vladimir.alexiev@ontotext.com>
2015-08-25 05:30:55 +0300

    rncfix.pl

---
+++ bat/rncfix.pl
@@ -0,0 +1,17 @@
+#!perl -wp0
+
+BEGIN {binmode (STDIN, ":crlf")}
+
+s{\t+}{ }g;
+s{^ +" "\n}{}gm;
+s{namespace ns_1 = "http://relaxng.org/ns/compatibility/annotations/1.0"\n}{}; # namespace a is the same
+s{ns_1:}{a:}g;
+s{( +)["'](.*)\\x\{a\}["'] ~}{$1##$2}g;
+s{(.*)\n +>> a:documentation \[\n((?: +##.*\n)+) *\]([,?]?)\n}{$2$1$3\n}g;
+         s{ *a:documentation \[\n((?: +##.*\n)+) *\]([,?]?)\n}{$1$2\n}g;
+s{^ *#* *\n}{}gm; # remove extraneous newlines
+s{\n *\}}{\}}g;
+s{([=\{]\n) *\n}{$1}g;
+s{\{ }{\{}g;
+s{ \}}{\}}g;
+s{(\n *[^#].*\n)(?=\w|#)(?!default|namespace)}{$1\n}g; # add newline before definition (word or comment starting at BOL)
